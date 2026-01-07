6a995345d2c85e77ea4fe04a4da04898614eb775
LabriP <abnorlapu@gmail.com>
2026-01-07 04:38:45 +0200

    Add title to FastAPI and new endpoint for powerbi

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="API Análisis Deportivo")
 
 @app.get("/")
-def root():
+def home():
     return {"message": "API funcionando"}
+
+@app.get("/powerbi")
+def powerbi_data():
+    return [
+        {"equipo": "Llaneros", "xG": 1.8, "tiros": 12},
+        {"equipo": "Millonarios", "xG": 2.1, "tiros": 15},
+        {"equipo": "Nacional", "xG": 0.9, "tiros": 8}
+    ]
